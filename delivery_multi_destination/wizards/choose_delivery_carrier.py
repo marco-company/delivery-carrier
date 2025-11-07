106cfb71056ed801a620dc07c8ae8b49c731cf82
hugues de keyzer <odoo@hugues.info>
2025-11-07 10:14:28 +0100

    [FIX] correctly handle error and warning

--- delivery_multi_destination/wizards/choose_delivery_carrier.py
+++ delivery_multi_destination/wizards/choose_delivery_carrier.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-from odoo import api, models
+from odoo import _, api, models
 
 
 class ChooseDeliveryCarrier(models.TransientModel):
@@ -13,6 +13,15 @@ class ChooseDeliveryCarrier(models.TransientModel):
         result = super()._onchange_carrier_id()
         if self.delivery_type == "base_on_destination":
             vals = self._get_shipment_rate()
+            # although ._onchange_carrier_id() in the delivery module can
+            # return a dict with an "error" key and a string value, this is
+            # not handled by the caller (see BaseModel._onchange_eval()),
+            # which only handles dicts with a "warning" key and a dict as a
+            # value.
             if vals.get("error_message"):
-                return {"error": vals["error_message"]}
+                return {
+                    "warning": {"title": _("Error"), "message": vals["error_message"]}
+                }
+            if vals.get("warning_message"):
+                return {"warning": {"message": vals["warning_message"]}}
         return result
